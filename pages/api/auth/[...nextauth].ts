6c50158d9e719effcc5127507cbb3d5133e60298
Vrishin Patel <39858602+Vrishtrix@users.noreply.github.com>
2022-07-09 08:22:40 +0000

    implement[auth]: NextAuth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export default NextAuth(
+{
+	secret: "QH7Bs8DudJmxWFHmaISndmawlj8/jQRMTotuW/HgzaA=",
+	
+	providers: [
+		CredentialsProvider(
+		{
+			name: "Credentials",
+
+			credentials: {
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "password" }
+			},
+
+			async authorize(credentials: { username: string; password: string })
+			{
+				if (credentials.username.toLowerCase() == "upeshp" && credentials.password === process.env.PASSWORD)
+				{
+					const user = { id: 1, name: "Upesh Patel", email: "upesh_patel@tamsinfotech.com" }
+					return user
+				}
+
+				return null
+			}
+		}
+		)
+	]
+}
+)
---
+++ pages/send_messages.tsx
@@ -0,0 +1,8 @@
+const SendMessages = () =>
+{
+	return (
+		<></>
+	)
+}
+
+export default SendMessages
