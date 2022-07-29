a2efadc4b791a607d2523d24d50eb397c795964f
Chiru B <33750251+itsezc@users.noreply.github.com>
2022-07-29 19:56:17 +0000

    fix: send_messages fetch

--- pages/send_messages.tsx
+++ pages/send_messages.tsx
@@ -1,12 +1,24 @@
+import * as React from 'react';
 import Link from "next/link"
 import { useSession } from "next-auth/react"
 import contactFetcher from "@util/contact_fetcher"
 
-const SendMessages = () =>
-{
-	const { data: session } = useSession()
-	const contacts = contactFetcher(process.env.USER_TOKEN)
-	
+interface IMessage {
+	id: string;
+	firstName: string;
+	lastName: string;
+};
+
+const SendMessages = () => {
+	const { data: session } = useSession();
+
+	const [contacts, setContacts] = React.useState<IMessage[]>([]);
+
+	React.useEffect(() => {
+		contactFetcher(process.env.NEXT_PUBLIC_USER_TOKEN)
+			.then(setContacts);
+	}, []);
+
 	if (!session) return <div className="flex flex:col gap:25 min-h:100vh ai:center jc:center"> Access denied. Please log in first. </div>
 
 	if (!contacts) return <div className="flex flex:col gap:25 min-h:100vh ai:center jc:center"> Failed to fetch contacts... </div>
@@ -14,9 +26,9 @@ const SendMessages = () =>
 	return (
 		<div className="flex flex:col gap:25 min-h:100vh ai:center jc:center">
 
-			{ contacts ? JSON.stringify(contacts) : "Invalid token provided." }
+			{contacts ? JSON.stringify(contacts) : "Invalid token provided."}
 			<Link href="/">
-				<button 
+				<button
 					className="block mx:auto b:2|solid|gray-50 r:5 p:0.75rem|1rem bg:gray-70:hover translateY(-2px):hover"
 				>
 					Go Back
